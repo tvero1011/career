01812ba5b46468c8327ef932a9f55580f67e7b1e
trev <t.vero1011@gmail.com>
2026-01-25 18:58:31 +0800

    fix: added requirements.txt and workflow structure

--- week05-Python-Review-automation/day2-boto3-S3-list/list_s3_buckets.py
+++ week05-Python-Review-automation/day2-boto3-S3-list/list_s3_buckets.py
@@ -5,11 +5,11 @@ def list_buckets():
     try:
         s3 = boto3.client("s3")
         response = s3.list_buckets()
-        # print(response)
+        print(response)
 
-     # print("S3 Buckets:")
-     # for bucket in response.get("Buckets", []):
-     #     print(f"- {bucket['Name']}")
+        print("S3 Buckets:")
+        for bucket in response.get("Buckets", []):
+            print(f"- {bucket['Name']}")
 
     except NoCredentialsError:
         print("AWS credentials not found.")
